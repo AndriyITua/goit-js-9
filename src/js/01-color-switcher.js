64bc43696c5d63f239cad0dcc8dcb274d137307e
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2022-01-21 19:41:39 +0200

    add

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,29 @@
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let intervalId = null;
+
+function changeColor() {
+  let color = getRandomColor();
+  refs.body.style.backgroundColor = color;
+}
+
+function startHandler() {
+  refs.startBtn.disabled = true;
+  intervalId = setInterval(changeColor, 1000);
+}
+
+function stopHandler() {
+  clearInterval(intervalId);
+  refs.startBtn.disabled = false;
+}
+
+refs.startBtn.addEventListener('click', startHandler);
+refs.stopBtn.addEventListener('click', stopHandler);
